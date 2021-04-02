25367d94ad9afa46a31eda700c937185a688d41c
Arpit Patel <Arpit2298@users.noreply.github.com>
2021-04-02 13:18:30 +0530

    latest

---
+++ Res_Portfolio.js
@@ -0,0 +1,31 @@
+$(function() {
+    var filterList = {
+        init: function() {
+            // MixItUp plugin
+            // http://mixitup.io
+            $('.portfolio-grid').mixItUp({
+                selectors: {
+                    target: '.portfolio',
+                    filter: '.filter'
+                },
+                load: {
+                    filter: 'all' // show app tab on first load
+                }
+            });
+        }
+    };
+    // Run the show!
+    filterList.init();
+})
+
+
+
+// .............................................................
+
+const toggle = document.querySelector('.menuOpen1 ')
+const navigation = document.querySelector('.menuEffects1')
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
